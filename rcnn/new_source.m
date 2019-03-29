81526ab2e86e573ecec9570d4875abd500a0e901
yyouan <b05202030@ntu.edu.tw>
2019-03-29 15:17:11 +0800

    update new source

--- rcnn/new_source.m
+++ rcnn/new_source.m
@@ -1,6 +1,6 @@
 path('.',path);
 root = folder_name();
-data = load('rcnn_weicheng_inside_add_one.mat');
+data = load('rcnn_data_all_2.mat');
 data = data.gTruth;
 oldPathDataSource = "C:\\Users\\lin\\Desktop\\matlab-dnn-contest-data";
 newPathDataSource = root;
